0e502bc6b21e1f1dd308e192096ff6d0539e78af
Шелкоуденко Аким Германович <ag_shelkoudenko@bingo-boom.com>
2019-03-01 11:55:17 +0300

    server config

--- test/test.mjs
+++ test/test.mjs
@@ -1,18 +1,24 @@
 import fs from "fs";
 import path from "path";
 import Mocha from "mocha";
+import ServerConfig from "../src/config.mjs";
+import DevServer from "../src/devserver.mjs";
 
 const mocha = new Mocha();
-const dirname = path.resolve(path.dirname(""));
+const config = new ServerConfig().config;
 
-const testDir = `${dirname}/test/tests`;
+const testDir = `${config.dirname}/test/tests`;
 fs.readdirSync(testDir)
   .filter(file => file.match(/\.js$/))
   .forEach(file => {
     mocha.addFile(path.join(testDir, file));
   });
 
-// const runner = mocha.timeout(30000).run();
-// runner.on("end", () => {
-//   process.exit();
-// });
+const devserver = new DevServer(config);
+devserver.precompile().then(() => {
+  // const runner = mocha.timeout(30000).run();
+  // runner.on("end", () => {
+  //   process.exit();
+  // });
+  devserver.run();
+});
